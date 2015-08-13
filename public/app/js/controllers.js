323d298de77dd52fcdc1e9077d2680da6c79a43c
bhavin_patel <bhavin.ibm@gmail.com>
2015-08-13 12:35:48 -0500

    dynamic css

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -3,9 +3,10 @@ var uuid = require('node-uuid');
 angular.isUndefinedOrNull = function(val) {
   return angular.isUndefined(val) || val === null
 }
+
 app.controller('cssBundleCtrl',['$scope','$css',function($scope,$css){
   // set the default bootswatch name
-  $scope.css = angular.isUndefinedOrNull(localStorage['theme'])? localStorage['theme'] :'darkly';
+  $scope.css = angular.isUndefinedOrNull(localStorage['theme'])? localStorage['theme'] :'cosmo';
   // create the list of bootswatches
   $scope.bootstraps = [
     { name: 'Light (cosmo)', url: 'cosmo' },
@@ -15,44 +16,41 @@ app.controller('cssBundleCtrl',['$scope','$css',function($scope,$css){
 
   $scope.CssCollection = [
     'css/material-design-color-palette.min.css',
-    'css/app.css',
     'css/animate.css',
     'lib/font-awesome/css/font-awesome.min.css',
     'lib/waves/waves.css',
     'css/booleanswitch.css',
-    'lib/angular-ui-switch/switch.css'
+    'lib/angular-ui-switch/switch.css',
+    'css/app.css'
   ];
 
   $scope.$watch('css',function(newval, oldval){
     console.log('new css = ' + newval);
-    if(newval == null) newval = 'darkly'; // default is darkly
-    localStorage['theme'] = newval;
+    if(newval == null) newval = 'darkly'; // default
 
     //remove old css collections
     $css.remove($scope.CssCollection);
-    $css.remove(['css/bootstrap.darkly.min.css',
+    $css.remove([
+      'css/bootstrap.darkly.min.css',
       'css/bootstrap.cosmo.min.css',
       'css/bootstrap.paper.min.css'
     ]);
 
-  //prepare for new  css collections
-  var newCssCollection = [];
-  newCssCollection.push('css/bootstrap.' + newval + '.min.css');
-  angular.forEach($scope.CssCollection,function(val){
-    newCssCollection.push(val);
-  });
-
-  //add new css collections
-  $css.add(newCssCollection);
-})
-
-
-
-
+    //prepare for new  css collections
+    var newCssCollection = [];
+    newCssCollection.push('css/bootstrap.' + newval + '.min.css');
+    angular.forEach($scope.CssCollection,function(val){
+      newCssCollection.push(val);
+    });
+    //add new css collections
+    $css.add(newCssCollection);
+  })
 }]);
+
 app.controller('indexCtrl',['$scope','windowFactory',function($scope,windowFactory){
   $scope.welcome = "eForms-nw.js";
 }]);
+
 app.controller('mainCtrl',['$scope','$state',function($scope,$state){
   $scope.today = new Date();
   $scope.format = 'M/d/yy h:mm:ss a';
@@ -62,17 +60,15 @@ app.controller('mainCtrl',['$scope','$state',function($scope,$state){
     $state.go('default',{type:'VIN',docid:uuid.v4()});
   };
 }]);
+
 app.controller('defaultCtrl',['$scope','$stateParams', '$state' ,function($scope,$stateParams,$state){
   $scope.hello = "Hello Default..!!";
   $scope.doctype = $stateParams.type;
   $scope.docid = $stateParams.docid;
   $scope.tabs = [];
-
-
   $scope.init = function() {
     $scope.addTab($scope.doctype);
   }
-
   $scope.addTab = function(type) {
     var d = new Date();
     var title = '' + type + ' - ' + d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear() + ' - ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
@@ -81,13 +77,13 @@ app.controller('defaultCtrl',['$scope','$stateParams', '$state' ,function($scope
   $scope.removeTab = function(index) {
     $scope.tabs.splice(index, 1);
   }
-
-
   $scope.init();
 }]);
+
 app.controller('applicationSettingsCtrl',['$scope',function($scope){
   //TODO: application settings related code.
 }]);
+
 app.controller('userSettingsCtrl',['$scope',function($scope){
   //TODO: user settings related code.
 }]);
