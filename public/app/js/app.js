c958979557a139d6b3e0cf7b1d422f1e312adb99
bhavin_patel <bhavin.ibm@gmail.com>
2015-08-17 10:38:38 -0500

    minor ui fixes

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -15,13 +15,6 @@ var app = angular.module('demoapp',[
 .run(['$window','$rootScope',function($window, $rootScope) {
   $rootScope.online = navigator.onLine;
 
-  // var path = './';
-  // var fs = require('fs');
-  //
-  // fs.watch(path, function() {
-  //   if (location)
-  //   location.reload();
-  // });
   $window.addEventListener("offline", function () {
     $rootScope.$apply(function() {
       $rootScope.online = false;
