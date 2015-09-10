5c13a9f7d94df0b778209efe01ede454199c2da5
Ryan <rthill91@gmail.com>
2015-09-10 11:07:10 -0500

    added queries skeleton

--- server/general/queries.js
+++ server/general/queries.js
@@ -4,12 +4,43 @@ var squel = require('squel');
 exports.SUBMIT_VIN_FORM_DATA = function(form) {
     console.log(form);
     return squel.insert();
-                // .into("Documents")
-                // .set("VINInspectionID")
-                // .set("DocumentID");
+                .into("Documents")
+                .set('VINInspectionID', 1)
+                .set('DocumentID', 1)
+                .set('TitleState', form.stateTitle)
+                .set('IsTitleorCourtNum', form.titleCourt)
+                .set('TitleOrCourtNum', form.titleCourtOrderNum)
+                .set('InspectionDateTime', form.inpsectionDateTime)
+                .set('WorkPhone', form.WorkPhone)
+                .set('AIRSWebServiceResponseID', 1)
+                .set('AIRSWebServiceRequestCounter', 1)
+                .set('AIRSWebServiceRequestStatusID', 1)
+                .set('AIRSWebServiceRequestNote', 1)
+                .set('AIRSWebServiceInvalidateNote', 1)
+                .set('Is25FeeCollected', form.feeCollected);
 }
 exports.SUBMIT_VIN_FORM_INFO = function(info) {
     console.log(info);
-    return sql.insert();
+    return squel.insert()
+                .into("VINInspections")
+                .set('DocumentID', info.id)
+                .set('TicketNum', info.title)
+                .set('CreateDate', info.createDate)
+                .set('Finalized', info.finalized)
+                .set('FinalizedDate', info.finalizedDate)
+                .set('Transferred', info.transferred)
+                .set('TransferredDate', info.transferredDate)
+                .set('StatusID', 1)
+                .set('StatusText', 1)
+                .set('TypeID', 1)
+                .set('TypeText', 1)
+                .set('CreateUserID', 1)
+                .set('OfficerName', 1)
+                .set('AgencyOri', 1)
+                .set('OfficerId', 1)
+                .set('Deleted', 1)
+                .set('LastModifiedDate', 1)
+                .set('Username', 1)
+                .set('VersionNumber', 1);
 }
 /* VIN (end) */
